15c7f0f2ae30dcc0c3ab6a5b75c1e776f5460e84
lalanikw <lalanikw@gmail.com>
2024-01-10 16:23:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <section class="wrapper">
+      <h1>NanGenLabs</h1>
+      <br></br>
+      <h2> Empowering the Future</h2>
+      <h3> Your Premier Destination for AI-driven Automation Solutions.</h3>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    <div><span class="dot"></span></div>
+    
+</section>
+  );
+}
+
+export default App;
